967748a548bc95a4d41e3bed72b99b5c22dc8508
Alexey Venderov <avenderov@gradle.com>
2022-12-14 21:53:52 +0100

    first commit

---
+++ src/test/java/com/github/avenderov/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.avenderov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    void alwaysPassing() {
+        assertTrue(true);
+    }
+}
